09ac2b931076db582e4480649ee4ac35ae62db38
Kristof Bohanec <bohanec.kristof@gmail.com>
2024-05-24 11:08:35 +0200

    Zacetek obrazca dodajanja polnilnic PMI-8

--- src/main/kotlin/view/components/content/AddingStationContent.kt
+++ src/main/kotlin/view/components/content/AddingStationContent.kt
@@ -6,14 +6,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 @Preview
 fun AddingStationContent() {
+    var station = remember { mutableStateOf("") }
+    var address = remember { mutableStateOf("") }
+
     //TODO
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -25,7 +34,20 @@ fun AddingStationContent() {
                 .wrapContentSize(align = Alignment.Center)
         ) {
             Text("ADD CHARGING STATION")
+            CustomTextField(station, "Enter station")
+            Text("ADD ADDRESS")
+            CustomTextField(address, "Enter address")
             //Todo obrazec
         }
     }
+}
+
+@Composable
+@Preview
+fun CustomTextField(state: MutableState<String>, placeholder: String) {
+    TextField(
+        value = state.value,
+        onValueChange = { state.value = it },
+        shape = RoundedCornerShape(12.dp),
+        placeholder = { Text(placeholder) })
 }
